7967c5b375259740d26faa7ae67ef7efee2a596b
Kshitij <79467163+3sterisk@users.noreply.github.com>
2023-05-15 09:39:02 +0530

    Added the button for resizing the cells aka div in canvas

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ console.log(cellSize);
 const canvas = document.querySelector(".canvas");
 
 function generateGrid(grid = gridSize, boxSize = cellSize) {
+  removeGrid();
   for (let i = 0; i < grid * grid; i++) {
     const div = document.createElement("div");
     div.className = "box";
@@ -16,12 +17,21 @@ function generateGrid(grid = gridSize, boxSize = cellSize) {
     canvas.appendChild(div);
   }
 }
-
 generateGrid();
+
+function removeGrid() {
+  canvas.innerHTML = "";
+}
 
 const boxes = document.querySelectorAll(".box");
 boxes.forEach((box) => {
   box.addEventListener("mouseover", (e) => {
     box.style.backgroundColor = "#fca311";
   });
+});
+
+const reSize = document.querySelector(".size-button");
+reSize.addEventListener("click", () => {
+  let size = +prompt("Enter the tiles you want on each side: ");
+  generateGrid(size, Math.floor(canvasSize / size));
 });
